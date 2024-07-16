6a22e9efed7322f4edf06e9e7db4eb8c3d6ebb46
dev <dev2@ritz.com.mm>
2024-07-16 13:37:43 +0630

    ADD: admin and role

---
+++ src/app/Models/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'email', 'password', 'phone', 'profile_picture', 'role_id',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+}
